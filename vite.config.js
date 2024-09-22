ac9a58f5e2910a8f010ca1722ea6297edc87ad55
Neji <newtonombese1@gmail.com>
2024-09-22 09:31:31 +0300

    Working on the configuration in to the configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import kumaUI from '@kuma-ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    kumaUI({
+      wasm: true,
+    })
+  ],
 })
