8769b3badb23774ce239cd87abda9653b653d314
Neji <newtonombese1@gmail.com>
2024-09-22 09:41:39 +0300

    Removing boilerplated css and also implementatioon of Headless UI component library

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Box, Heading, css, Spacer, Flex, Text, Button } from "@kuma-ui/core";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box as="main" display="flex" flexDir={["column", "row"]}>
+      <Heading
+        as="h3"
+        className={css`
+          color: red;
+          @media (max-width: sm) {
+            color: blue;
+          }
+        `}
+      >
+        Kuma UI
+      </Heading>
+      <Spacer size={4} />
+      <Flex flexDir={`column`}>
+        <Text as="p" fontSize={24}>
+          Headless UI Component Library
+        </Text>
+        <Button>Getting Started</Button>
+      </Flex>
+    </Box>
+  );
 }
 
-export default App
+export default App;
