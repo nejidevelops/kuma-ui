24d0aa34eeaa914e00eba174e01591b8afc413d3
Neji <newtonombese1@gmail.com>
2024-09-22 09:46:20 +0300

    Setting up Hello World for starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { Box, Heading, css, Spacer, Flex, Text, Button } from "@kuma-ui/core";
+import { Box, Heading } from "@kuma-ui/core";
 function App() {
   return (
-    <Box as="main" display="flex" flexDir={["column", "row"]}>
-      <Heading
-        as="h3"
-        className={css`
-          color: red;
-          @media (max-width: sm) {
-            color: blue;
-          }
-        `}
-      >
-        Kuma UI
+    <Box bgColor="red" height="100vh" display="flex" justifyContent="center" alignItems="center">
+      <Heading color="white" fontSize="120px">
+        Hello
       </Heading>
-      <Spacer size={4} />
-      <Flex flexDir={`column`}>
-        <Text as="p" fontSize={24}>
-          Headless UI Component Library
-        </Text>
-        <Button>Getting Started</Button>
-      </Flex>
     </Box>
   );
 }
