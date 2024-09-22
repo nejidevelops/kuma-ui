397d37f16228bf60fc80dcada614bf25c657cde4
Neji <newtonombese1@gmail.com>
2024-09-22 09:56:20 +0300

    Testing stylings using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { Box, Heading } from "@kuma-ui/core";
 function App() {
   return (
-    <Box bgColor="red" height="100vh" display="flex" justifyContent="center" alignItems="center">
-      <Heading color="white" fontSize="120px">
-        Hello
+    <Box
+      // as="h1" // Use to alter the rendered element to any HTML element a user defines
+      bgColor="red"
+      height="auto"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      m={64}
+      p={24}
+      _hover={{
+        bgColor: "green"
+      }}
+    >
+      <Heading color="brown" p={32} bgColor="yellow" fontSize="120px">
+        Hello World
       </Heading>
     </Box>
   );
